a8b1b2c52aa3a8fc15ddbb5c2800ef9bf873aa4d
Ankita Bantey <38222723+ankitabantey@users.noreply.github.com>
2020-11-08 05:55:38 -0600

    Working pages

---
+++ src/Pages/Contact.js
@@ -0,0 +1,13 @@
+import './Contact.css';
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Contact() {
+    return (
+        <div className = "Contact">
+            <h2>Civic Info API is called on this page</h2>
+        </div>
+    );
+}
+
+export default Contact;
